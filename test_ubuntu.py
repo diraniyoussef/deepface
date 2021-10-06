d683efd858d6ab7842297afe4ded44efa204e87e
diraniyoussef <diraniyoussef@hotmail.com>
2021-10-06 10:00:37 +0300

    bug fixed. Two weird bugs actually and both related to process_frame. One of them is that FaceDetector.detect_faces produces a face with 0 dimension in shape[1] e.g.. The other one is that FaceDetector.detect_faces produces a good face but inside process_face when calling functions.preprocess_face it is not able to find it !

--- test_ubuntu.py
+++ test_ubuntu.py
@@ -5,9 +5,11 @@
 #DeepFace.enhanced_stream(db_path = '/home/youssef/PythonProjects/AI/DeepFace_Project/database2', source = '/home/youssef/PythonProjects/AI/DeepFace_Project/make a video/teta image.mp4', actions = [], number_of_processes = 1)
 #  
 if __name__ == "__main__":
-	from deepface import instructions
+	#from deepface import instructions
 	from deepface import DeepFace
 
-	instructions.instructions()
-	DeepFace.enhanced_stream(db_path = '/home/youssef/PythonProjects/AI/DeepFace_Project/database1', source = '/home/youssef/PythonProjects/AI/DeepFace_Project/make a video/6seconds.mp4', actions = [], number_of_processes = 2, auto_add = True)
-	#DeepFace.play_with_annotations("C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/make a video/trial2.mp4", "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database/frames_info_trial2.pkl", speed = "slow", fps = 30, im_size = (960, 540))
+	#instructions.instructions()
+	#DeepFace.enhanced_stream(db_path = '/home/youssef/PythonProjects/AI/DeepFace_Project/database1', source = '/home/youssef/PythonProjects/AI/DeepFace_Project/make a video/me.mp4', model_name = "VGG-Face", detector_backend="dlib", actions = [], number_of_processes = 2, auto_add = True, normalization = "base", distance_metric = 'cosine')
+	DeepFace.play_with_annotations("C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/make a video/me.mp4", "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database/frames_info_me.pkl", speed = "slow", fps = 30, im_size = (960, 540))
+
+

78eae4be0ab135df291649e7922d0317e0e02745
Youssef Dirani <diraniyoussef@gmail.com>
2021-10-15 13:38:06 +0300

    testing... It works but the performance of recognition is so bad in VGG-Face and less bad in DeepFace

--- test1.py
+++ test1.py
@@ -1,2 +1,7 @@
+#from deepface import DeepFace
+#DeepFace.enhanced_stream(db_path= "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database6", source= "https://www.youtube.com/watch?v=z-BgpfYb_uU", source_type="youtube", actions = ['emotion'], auto_add = True, model_name ='DeepFace')
+
 import test_win
-test_win.search_stream("C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/empty db/auto_add", "https://www.youtube.com/watch?v=NIk1-ck4c6Q&ab_channel=WOWENGLISH", "youtube", emotions= True)
+#test_win.play_with_annotations("https://www.youtube.com/watch?v=z-BgpfYb_uU", "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database6/frames_info_prime minister of pakistan imran khan exclusive interview on bbc news.pkl", video_place= "youtube")
+
+test_win.play_with_annotations("C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/make a video/1.mp4", "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database4/frames_info_1.pkl", audio= True)
--- test_win.py
+++ test_win.py
@@ -1,27 +1,28 @@
 #from deepface import instructions
 
-def search_stream(images_folder, video_file, video_place = "disk", processing_video_size = (), emotion = False, number_of_processes = 2, auto_add = False):
+def search_stream(images_folder, video_file, video_place = "disk", processing_video_size = (), emotion = False, number_of_processes = 2, auto_add = False, model_name = 'DeepFace'):
 	from deepface import DeepFace
 	if emotion:
 		actions = ['emotion']
 	else:
 		actions = []
-	DeepFace.enhanced_stream(db_path = images_folder, source = video_file, source_type = video_place, processing_video_size = processing_video_size, model_name = "VGG-Face", detector_backend="dlib", actions = actions, number_of_processes = number_of_processes, auto_add = auto_add, normalization = "base", distance_metric = 'cosine')
+	DeepFace.enhanced_stream(db_path = images_folder, source = video_file, source_type = video_place, processing_video_size = processing_video_size, model_name = model_name, detector_backend="dlib", actions = actions, number_of_processes = number_of_processes, auto_add = auto_add, normalization = "base", distance_metric = 'cosine')
 
 def prepend_images_names(images_folder, person_name):
 	from deepface import DeepFace
 	DeepFace.prepend_imgs_names(imgs_path=images_folder, name = person_name)
 
-def play_with_annotations(video_file, info_file, speed = "normal", video_place = "disk", processing_video_size = (), output_video_size = (), audio = False):
+def play_with_annotations(video_file, info_file, speed = "normal", video_place = "disk", processing_video_size = (), output_video_size = (), audio = False, color= "dark_gray"):
 	from deepface import DeepFace
-	DeepFace.play_with_annotations(video_file, info_file, speed= speed, source_type= video_place, processing_video_size= processing_video_size, output_video_size= output_video_size, audio= audio)
+	DeepFace.play_with_annotations(video_file, info_file, speed= speed, source_type= video_place, processing_video_size= processing_video_size, output_video_size= output_video_size, audio= audio, color= color)
 
 
 if __name__ == "__main__":
 	pass
-	#we won't care much for the slash or back-slash in the paths (especially the \U), since they'll be given by the user as string inputs, so they'll be probably well encoded
+	#test_win.play_with_annotations("C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/make a video/1.mp4", "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database4/frames_info_1.pkl")
+	#test_win.search_stream("C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database6", "https://www.youtube.com/watch?v=z-BgpfYb_uU", video_place="youtube", auto_add = True)
+	#test_win.play_with_annotations("https://www.youtube.com/watch?v=z-BgpfYb_uU", "C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database6/frames_info_prime minister of pakistan imran khan exclusive interview on bbc news.pkl", video_place= "youtube")
 
-	#test_win.search_stream("C:\Users\Dirani\ProgrammingProjects\DeepFace_Project\database6", "https://www.youtube.com/watch?v=z-BgpfYb_uU", video_place="youtube", auto-add = True)
 
 	#instructions.instructions()
 	#DeepFace.enhanced_stream(db_path = 'C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/database', source = 'C:/Users/Dirani/ProgrammingProjects/DeepFace_Project/make a video/الأهل و التلاميذ و الفصحى.mp4', processing_video_size = (), model_name = "VGG-Face", detector_backend="dlib", actions = [], number_of_processes = 2, auto_add = True, normalization = "base", distance_metric = 'cosine') #detector_backend="dlib", detector_backend="mtcnn"
